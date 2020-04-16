7ca445687c6759ce3d84209b3e98f1b4a9c69f14
John T. Foster <johntfosterjr@gmail.com>
2020-04-16 08:52:32 -0500

    First commit

---
+++ assignment17.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import numpy as np
+
+from PyTrilinos import Epetra
+
+class Max():
+
+    def __init__(self, basename, comm):
+
+        self.comm = comm
+        self.rank = comm.MyPID()
+        self.size = comm.NumProc()
+
+
+
+    def get_max(self):
+                             
+        return #maximum value across all procs
+
+
+
+
+if __name__ == "__main__":
+
+    from PyTrilinos import Epetra
+
+    comm = Epetra.PyComm()
+
+    T = Max('ss', comm)
+
+    print(T.get_max())
